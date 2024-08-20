26ec9243b0e63e4ee17dd79fe925a88d5c2df2ef
Isabella <idavila2021@gmail.com>
2024-08-20 18:44:57 +0100

    feat: get req

--- server.js
+++ server.js
@@ -1,51 +1,56 @@
-import express from 'express';
-import * as http from 'node:http';
-import { Server } from 'socket.io';
+import express from "express";
+import * as http from "node:http";
+import { Server } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST']
-  }
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the WebSocket Cursors App!");
+});
+
 const cursors = new Map();
 
-io.on('connection', socket => {
-  console.log('A user connected:', socket.id);
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
 
   // Send all existing cursors to the new client
-  socket.emit('allCursors', Array.from(cursors.entries()));
+  socket.emit("allCursors", Array.from(cursors.entries()));
 
-  socket.on('cursorMove', data => {
+  socket.on("cursorMove", (data) => {
     // Ensure x and y are between 0 and 1
     const x = Math.max(0, Math.min(1, data.x));
     const y = Math.max(0, Math.min(1, data.y));
 
     cursors.set(socket.id, { x, y });
     // Broadcast to all clients except the sender
-    socket.broadcast.emit('cursorUpdate', {
+    socket.broadcast.emit("cursorUpdate", {
       id: socket.id,
       x,
-      y
+      y,
     });
-    console.log('Cursor moved:', socket.id, { x, y });
+    console.log("Cursor moved:", socket.id, { x, y });
   });
-  socket.on('click', data => {
+  socket.on("click", (data) => {
     console.log(
       `Click from user ${socket.id} at position (${data.x}, ${data.y})`
     );
-    io.emit('userClicked', { userId: socket.id, x: data.x, y: data.y });
+    io.emit("userClicked", { userId: socket.id, x: data.x, y: data.y });
   });
 
-  socket.on('disconnect', () => {
-    console.log('A user disconnected:', socket.id);
-    io.emit('cursorRemove', socket.id);
+  socket.on("disconnect", () => {
+    console.log("A user disconnected:", socket.id);
+    io.emit("cursorRemove", socket.id);
   });
 });
 
-const PORT = process.env.PORT || 4000; 
+const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, "0.0.0.0", () => {
   console.log(`Socket.IO server running on port ${PORT}`);
